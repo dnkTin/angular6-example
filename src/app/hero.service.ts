4e633627f176e23657755ff1dc7e790ddbaebd52
Tin Dang Ngoc Khanh <dnktin@aavn.local>
2018-06-04 13:36:59 +0700

    Add example file

---
+++ src/app/hero.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+import { Hero } from './heroes/hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+	getHeroes(): Observable<Hero[]> {
+		// TODO: send the message _after_ fetching the heroes
+		this.messageService.add('HeroService: fetched heroes');
+		return of(HEROES);
+	} 
+
+// Angular will inject the singleton MessageService into that property when it creates the HeroService.
+  constructor(private messageService: MessageService) { }
+
+
+}
